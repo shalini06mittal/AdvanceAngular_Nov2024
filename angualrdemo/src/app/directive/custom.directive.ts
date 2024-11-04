3b80967d29554f97dbeb5f48937237057ad78a05
Shalini Mittal <shalini06.mittal@gmail.com>
2024-11-04 08:44:04 +0530

    labs

--- angualrdemo/src/app/directive/custom.directive.ts
+++ angualrdemo/src/app/directive/custom.directive.ts
@@ -1,41 +1,38 @@
+import { AnimationBuilder } from '@angular/animations';
 import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appCustom]'
+  selector: '[hover]'
 })
-export class CustomDirective implements OnInit{
+export class CustomDirective {//implements OnInit{
 
 
-  @HostBinding("src")
-  imgSrc:string | null = null;
+  @Input() 
+  hover: string | null = null;
 
-  @Input()
-  name!:string;
+  @HostBinding('style.backgroundColor') 
+  backgroundColor: string ='';
 
-  constructor(private el:ElementRef, private renderer:Renderer2) {
-    console.log(el);
-    renderer.setStyle(el.nativeElement,'color','green');
-    renderer.setStyle(el.nativeElement,'border','3px solid black');
-    
-   }
-  ngOnInit(): void {
-    this.renderer.setAttribute(this.el.nativeElement,'title', this.name);
-    this.imgSrc = `assets/${this.name}.jpg`;
+  @HostBinding('style.transition') 
+  trans: string ='';
+
+  private tooltipElement: HTMLElement;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.tooltipElement = this.renderer.createElement('div');
+    this.renderer.addClass(this.tooltipElement, 'tooltip1');
+    this.trans = '1s ease-in-out';
+  }
+  @HostListener('mouseenter') 
+  onMouseEnter() {
+    this.tooltipElement.textContent = this.hover;
+    this.backgroundColor = 'lightgreen';
+    this.renderer.appendChild(this.el.nativeElement, this.tooltipElement);
+  }
+  @HostListener('mouseleave') 
+  onMouseLeave() {
+    this.renderer.removeChild(this.el.nativeElement, this.tooltipElement);
+    this.backgroundColor = 'white';
   }
 
-
-   @HostListener("mouseenter")
-   m1(){
-    
-    this.renderer.setStyle
-    (this.el.nativeElement,'backgroundColor','yellow');
-    this.imgSrc = 'assets/heart.jpg';
-   }
-
-   @HostListener("mouseleave")
-   m2(){
-    this.renderer.setStyle
-    (this.el.nativeElement,'backgroundColor','white');
-    this.imgSrc = `assets/${this.name}.jpg`;
-   }
-}
+  }
